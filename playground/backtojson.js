89665dab4e959bde7269f24e37ac77f18bf32a61
Peter Linders <plinders@gmail.com>
2018-09-04 15:32:43 +0200

    nested arrays with all data. Ready to make JSON

--- playground/backtojson.js
+++ playground/backtojson.js
@@ -2,28 +2,18 @@ const showdown = require('showdown');
 const fs = require('fs');
 const JFile = require('jfile');
 
+const parseMD = require('../parseMD.js')
+
 
 showdown.setFlavor('github');
 showdown.setOption('tables', true);
 
-// const inputString = fs.readFileSync('input.md', 'UTF8');
-
 const file = new JFile('input.md');
-// console.log(file.lines);
 
+// var sections = parseMD.splitH2Headings(file);
+// console.log(sections);
 
-file.lines.forEach((line, index) => {
-    if (line.match(/(^## )/m)) {
-        console.log(index, line);
-    }
-})
+var title = parseMD.getTitle(file);
+console.log(title);
 
-
-
-// console.log(lines);
-
-
-//var converter = new showdown.Converter();
-
-//var html = converter.makeHtml(inputString);
 
